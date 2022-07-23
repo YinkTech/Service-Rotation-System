57fadb2c3b0b22289b18f5f26fa9d188a0cd3bd1
Olayinka Bidemi Ayeni <65237847+YinkTech@users.noreply.github.com>
2022-07-23 18:42:44 +0100

    add styling

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,102 @@
-home = document.getElementById('home');
-about = document.getElementById('about');
 
 let sideNav = document.querySelector('.sidebar');
 let headers = document.querySelector('.header');
+let content = document.querySelector('.content');
+let navOpen = document.querySelector('.bi-menu-button-wide');
+let navcolse = document.querySelector('.bi-menu-button');
+
+navcolse.style.display = 'none';
 
 
 const opennav = () => {
     sideNav.style.display = 'block';
+    navOpen.style.display = 'none';
+    navcolse.style.display = 'block';
 }
 
-function forthSectionShow() {
-    home.style.display = 'none';
-    about.style.display = 'block';
+const closenav = () => {
+    sideNav.style.display = 'none';
+    navcolse.style.display = 'none';
+    navOpen.style.display = 'block';
 }
+
+
+home = document.getElementById('home');
+pOfficers = document.getElementById('pOfficers');
+nominal = document.getElementById('nominal');
+retiment = document.getElementById('retiment');
+dueRetirement = document.getElementById('dueRetirement');
+management = document.getElementById('management');
 
 home.style.display = 'block';
-about.style.display = 'none';
+pOfficers.style.display = 'none';
+nominal.style.display = 'none';
+retiment.style.display = 'none';
+dueRetirement.style.display = 'none';
+management.style.display = 'none';
+
+function firstShow() {
+    home.style.display = 'block';
+    pOfficers.style.display = 'none';
+    nominal.style.display = 'none';
+    retiment.style.display = 'none';
+    dueRetirement.style.display = 'none';
+    management.style.display = 'none';
+}
+
+
+function secondShow() {
+    home.style.display = 'none';
+    pOfficers.style.display = 'block';
+    nominal.style.display = 'none';
+    retiment.style.display = 'none';
+    dueRetirement.style.display = 'none';
+    management.style.display = 'none';
+}
+
+
+function thirdShow() {
+    home.style.display = 'none';
+    pOfficers.style.display = 'none';
+    nominal.style.display = 'block';
+    retiment.style.display = 'none';
+    dueRetirement.style.display = 'none';
+    management.style.display = 'none';
+}
+
+
+function forthShow() {
+    home.style.display = 'none';
+    pOfficers.style.display = 'none';
+    nominal.style.display = 'none';
+    retiment.style.display = 'block';
+    dueRetirement.style.display = 'none';
+    management.style.display = 'none';
+}
+
+function fifthShow() {
+    home.style.display = 'none';
+    pOfficers.style.display = 'none';
+    nominal.style.display = 'none';
+    retiment.style.display = 'none';
+    dueRetirement.style.display = 'block';
+    management.style.display = 'none';
+}
+
+function sixShow() {
+    home.style.display = 'none';
+    pOfficers.style.display = 'none';
+    nominal.style.display = 'none';
+    retiment.style.display = 'none';
+    dueRetirement.style.display = 'none';
+    management.style.display = 'block';
+}
+
+
 
 // Add active class to the current button (highlight it)
-var head = document.getElementById("myDIV");
-var btns = head.getElementsByClassName("btn");
+var headnav = document.getElementById("navDIV");
+var btns = headnav.getElementsByClassName("btn");
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
   var current = document.getElementsByClassName("active");
@@ -27,4 +104,3 @@ for (var i = 0; i < btns.length; i++) {
   this.className += " active";
   });
 }
-  btns.style.display = 'none';
